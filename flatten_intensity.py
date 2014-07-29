07a06bdbeddc74df2f3fc7f0e29b78384f558f7b
Rachel Levanger <rachel.levanger@gmail.com>
2014-07-29 04:46:54 -0400

    Finished adding code to flatten_image.py. Note: changes depend on nonexistent file convert_color_mode.py, which stil needs to be written. Was using colorsys to convert colors, but this did not produce the desired effect.

--- flatten_intensity.py
+++ flatten_intensity.py
@@ -1,6 +1,7 @@
-from scipy import misc
-import colorsys
 from __future__ import division
+
+from scipy import misc
+import convert_color_mode as ccm
 from optparse import OptionParser
 
 
@@ -19,14 +20,26 @@ parser.add_option("-i",
 img = misc.imread(options.imagefile)
 
 # For each pixel (top to bottom, left to right):
-for x in [0:img.size[0]]
-    for y in [0:img.size[1]]
+for x in range(0,img.shape[0]):
+    for y in range(0,img.shape[1]):
         # Convert rgb to hls and modify the l attribute to
         # the intensity given by the user. Reconvert back to
         # rgb and write back to the image object on this pixel
         
+        rgb = img[x,y,:]/255
+
+        hls = list(ccm.rgb_to_his(rgb[0],rgb[1],rgb[2]))
+
+        his[1] = float(options.intensity)
+
+        rgb = list(ccm.his_to_rgb(his[0],his[1],his[2]))
+
+        rgb = [int(i*255) for i in rgb]
+
+        img[x,y,:] = rgb
+
 
 # Save the modified image to a new file.
-
+misc.imsave('hoopoe_intensity_%s.jpg' % options.intensity, img)
 
 
